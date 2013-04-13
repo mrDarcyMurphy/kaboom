1c186eade18eb5f19124250912a768bcdd7605a1
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-04-13 17:03:15 -0500

    setup stack tests

---
+++ test/stack.js
@@ -0,0 +1,8 @@
+var assert = require('assert')
+var stack = require('../lab/stack.js')
+
+describe('stack', function(){
+  it('exists', function(){
+    assert(stack, "stack doesn't exist")
+  })
+})
