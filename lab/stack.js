a0aef7aa657af16ec9075839fe1c5521ad5abd0c
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-04-13 17:16:48 -0500

    implemented push

--- lab/stack.js
+++ lab/stack.js
@@ -1,3 +1,11 @@
-module.exports = function() {
+var stack = []
 
-}
+var Stack = function() {}
+
+Stack.prototype.push = function(i) {
+  stack.push(i);
+  this.length = stack.length;
+  return stack.length;
+};
+
+module.exports = Stack
--- test/stack.js
+++ test/stack.js
@@ -1,8 +1,32 @@
 var assert = require('assert')
-var stack = require('../lab/stack.js')
+var Stack = require('../lab/stack.js')
 
 describe('stack', function(){
-  it('exists', function(){
-    assert(stack, "stack doesn't exist")
+
+  var stack
+
+  before(function(){
+    stack = new Stack()
   })
+
+  it('should be able to initialize', function(){
+    assert(stack, "stack doesn't exist")
+    assert(stack instanceof Stack, "stack is not an instanceof Stack")
+  })
+
+
+
+  describe('push method', function(){
+
+    it('should push a new item onto the stack', function(){
+      assert(stack.push('foo'))
+    })
+
+    it('sets length to new stack length', function(){
+      assert.equal(stack.length, 1)
+    })
+
+  })
+
+
 })
