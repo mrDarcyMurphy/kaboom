a9b77b46da289932fcecc19be807a2601283d45d
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-04-13 17:28:08 -0500

    set length on init

--- lab/stack.js
+++ lab/stack.js
@@ -1,9 +1,10 @@
 var Stack = function(stack) {
-  this.stack = Array.isArray(stack) ? stack : [];
+  this.stack = Array.isArray(stack) ? stack : []
+  this.length = this.stack.length
 }
 
 Stack.prototype.push = function(i) {
-  return this.length = this.stack.push(i);
+  return this.length = this.stack.push(i)
 };
 
 Stack.prototype.length = 0
