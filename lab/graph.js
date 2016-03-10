3581faf8762ac04e2d76048129235c8299b033f1
Darcy Murphy <mrDarcyMurphy@gmail.com>
2016-03-10 13:23:10 -0600

    stub Graph

---
+++ lab/graph.js
@@ -0,0 +1,15 @@
+var Graph = function(v, e) {
+}
+
+Graph.prototype.MakeGraph = function MakeGraph() { }
+Graph.prototype.Vertices = function Vertices() { }
+Graph.prototype.Edges = function Edges() { }
+Graph.prototype.Neighbours = function Neighbours() { }
+Graph.prototype.AddVertex = function AddVertex() { }
+Graph.prototype.AddDirectedEdge = function AddDirectedEdge() { }
+Graph.prototype.AddUndirectedEdge = function AddUndirectedEdge() { }
+Graph.prototype.DeleteVertex = function DeleteVertex() { }
+Graph.prototype.DeleteEdge = function DeleteEdge() { }
+Graph.prototype.MakeGraph = function MakeGraph() { }
+
+module.exports = Graph
